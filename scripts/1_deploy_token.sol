5aadc3a7a200e37516972d1524760a626c4e9de1
jayakumar9 <93363487+jayakumar9@users.noreply.github.com>
2021-11-03 13:41:28 +0530

    saved at video time 3.solidity 1:04:44

--- scripts/1_deploy_token.sol
+++ scripts/1_deploy_token.sol
@@ -1,1 +1,16 @@
 # create this file under ERC20_BROWNIE/scripts-video time 3.solidity 1:02:49
+
+from brownie import OurToken
+from scripts.helpful_scripts import get_account
+from web3 import web3
+
+
+initial_supply=web3.toWei(1000,"ether")
+
+
+def main():
+    account=get_account()
+    our_token=OurToken.deploy(initial_supply.{"from":account})
+    print(our_token.name())
+    
+    
